6fed68885d1830ec4cc887bde82604610b38aa8d
Thommelise <Thomsen91@outlook.com>
2019-06-06 14:39:34 +0200

    Merge branch 'master' of C:\Users\ASUS\IdeaProjects\Semester-Projekt-2 with conflicts.

--- src/main/view/personale/LoginController.java
+++ src/main/view/personale/LoginController.java
@@ -1,17 +1,15 @@
 package main.view.personale;
 
-import javafx.scene.control.Button;
+import javafx.application.Platform;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import main.control.Client;
 import main.control.UI;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
 import main.handler.MedarbejderHandler;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 
@@ -24,10 +22,9 @@ public class LoginController {
     @FXML
     private TextField errorMessage;
 
-    @FXML
-    private Button closeBtn;
-
     private MedarbejderHandler medarbejderHandler = MedarbejderHandler.initMedarbejderHandler();
+
+
 
         @FXML
         void LoginBnt (javafx.event.ActionEvent event) {
@@ -38,7 +35,6 @@ public class LoginController {
                 }
                 if (stilling.equals("Kok")) {
                     try {
-                        Client.setStilling("Kok");
                         URL kokBnt = getClass().getResource("/main/view/kok/Kok.fxml");
                         BorderPane kokSceen = FXMLLoader.load(kokBnt);
                         BorderPane borderPane = UI.getRoot();
@@ -50,7 +46,6 @@ public class LoginController {
 
                 if (stilling.equals("Chef")) {
                     try {
-                        Client.setStilling("Chef");
                         URL restaurantChefBnt = getClass().getResource("/main/view/restaurantchef/RestaurantChef.fxml");
                         BorderPane restaurantChefSceen = FXMLLoader.load(restaurantChefBnt);
                         BorderPane borderPane = UI.getRoot();
@@ -62,7 +57,6 @@ public class LoginController {
 
                 if (stilling.equals("Personale")) {
                     try {
-                        Client.setStilling("Personale");
                         URL personaleBnt = getClass().getResource("/main/view/personale/BestilleHovedRet.fxml");
                         BorderPane personaleSceen = FXMLLoader.load(personaleBnt);
                         BorderPane borderPane = UI.getRoot();
@@ -75,11 +69,4 @@ public class LoginController {
                 errorMessage.setText("Skal være din id");
             }
         }
-
-    @FXML
-    void closeApp (javafx.event.ActionEvent event) {
-
-        Stage stage = (Stage) closeBtn.getScene().getWindow();
-        stage.close();
-    }
 }
